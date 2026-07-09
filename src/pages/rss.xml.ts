453ecca021f7940b56f8c13cba7d5a6ffc1cbc1d
misty <sakura.erii.suki@gmail.com>
2026-07-09 14:12:58 +0800

    fix: add atom:link self reference to RSS feed, point discovery to rss.xml

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,9 +1,56 @@
+import { getCollection, type CollectionEntry } from "astro:content";
+import { getPath } from "@/utils/getPath";
+import getSortedPosts from "@/utils/getSortedPosts";
 import { SITE } from "@/config";
 
+function escapeXml(str: string): string {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+function formatRssDate(date: Date): string {
+  return date.toUTCString();
+}
+
 export async function GET() {
+  const posts = await getCollection("blog");
+  const sortedPosts = getSortedPosts(posts);
   const siteUrl = SITE.website.replace(/\/$/, "");
-  return new Response(null, {
-    status: 301,
-    headers: { Location: `${siteUrl}/atom.xml` },
+
+  const items = sortedPosts
+    .map((post) => {
+      const { data } = post;
+      const path = getPath(post.id, post.filePath);
+      const url = `${siteUrl}${path}`;
+      const pubDate = new Date(data.modDatetime ?? data.pubDatetime);
+
+      return `
+    <item>
+      <title>${escapeXml(data.title)}</title>
+      <link>${escapeXml(url)}</link>
+      <guid isPermaLink="true">${escapeXml(url)}</guid>
+      <description>${escapeXml(data.description)}</description>
+      <pubDate>${formatRssDate(pubDate)}</pubDate>
+    </item>`;
+    })
+    .join("");
+
+  const feed = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>${escapeXml(SITE.title)}</title>
+    <description>${escapeXml(SITE.desc)}</description>
+    <link>${escapeXml(siteUrl)}/</link>
+    <atom:link href="${escapeXml(siteUrl)}/rss.xml" rel="self" type="application/rss+xml"/>
+    ${items}
+  </channel>
+</rss>`;
+
+  return new Response(feed, {
+    headers: { "Content-Type": "application/rss+xml; charset=utf-8" },
   });
 }
