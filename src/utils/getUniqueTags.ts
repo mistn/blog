98424d046469aee4fc4946b0a0e80fd1b2dec4d6
misty <sakura.erii.suki@gmail.com>
2026-07-22 16:50:13 +0800

    feat: show tags with post count on search page

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -2,22 +2,29 @@ import type { CollectionEntry } from "astro:content";
 import { slugifyStr } from "./slugify";
 import postFilter from "./postFilter";
 
-interface Tag {
+export interface Tag {
   tag: string;
   tagName: string;
+  count: number;
 }
 
 const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
-  const tags: Tag[] = posts
+  const all = posts
     .filter(postFilter)
     .flatMap(post => post.data.tags)
-    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
-    .filter(
-      (value, index, self) =>
-        self.findIndex(tag => tag.tag === value.tag) === index
-    )
-    .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
-  return tags;
+    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }));
+
+  const seen = new Map<string, Tag>();
+
+  for (const { tag, tagName } of all) {
+    if (seen.has(tag)) {
+      seen.get(tag)!.count++;
+    } else {
+      seen.set(tag, { tag, tagName, count: 1 });
+    }
+  }
+
+  return [...seen.values()].sort((a, b) => a.tag.localeCompare(b.tag));
 };
 
 export default getUniqueTags;
